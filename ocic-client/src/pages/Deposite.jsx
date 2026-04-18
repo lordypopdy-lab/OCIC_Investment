ab3c5fe720f26d9b93573e31d4c0674ab576d895
James Isaac <deelordpopdy2@gmail.com>
2026-04-18 19:16:16 +0100

    Update Deposite.jsx
    Safe

--- ocic-client/src/pages/Deposite.jsx
+++ ocic-client/src/pages/Deposite.jsx
@@ -14,6 +14,8 @@ const Deposite = () => {
     const walletConnect = async () => {
         toast.success("Wallet Connect Comming Soon!")
     }
+    //0x36c236529cf2579ccc06b450c6b1827efa5eeb8aaf8d7d49f4906309be2e070e
+    //12mndXTkUuc8Guj9h7sWUNdZaKpGtimsiW
     return (
         <>
             <MainNavBar />
@@ -26,7 +28,7 @@ const Deposite = () => {
                                     <button onClick={walletConnect} className='btn btn-secondary w-100'><i className="fas fa-wallet text-warning m-2"></i>Connect Wallet</button>
                                     <div style={{ border: "none", borderRadius: "9px" }} className="card card-gradient">
                                         <div className="card-body card-gradient">
-                                        <h5 onClick={() => handleCopy("12mndXTkUuc8Guj9h7sWUNdZaKpGtimsiW")}>12mndXTkUuc8Guj9h7sWU...<i style={{ cursor: "pointer" }} className="fas fa-copy text-warning m-1"></i></h5>
+                                        <h5 onClick={() => handleCopy("1GNu6gMQMR2rWxZubMQ46icSSuTUbrNZLZ")}>12mndXTkUuc8Guj9h7sWU...<i style={{ cursor: "pointer" }} className="fas fa-copy text-warning m-1"></i></h5>
                                             <h4 className="card-title">Deposit Using Bitcoin | USDT(TRC20)</h4>
                                             <p className="card-description">Bitcoin | USDT Deposit Method</p><hr />
                                             <form className="forms-sample">
@@ -38,7 +40,7 @@ const Deposite = () => {
                                 <div className="col-xl-6 col-sm-6 grid-margin mt-5">
                                     <div style={{ border: "none", borderRadius: "9px" }} className="card card-gradient">
                                         <div className="card-body card-gradient">
-                                        <h5 onClick={() => handleCopy("0x36c236529cf2579ccc06b450c6b1827efa5eeb8aaf8d7d49f4906309be2e070e")}>0x36c236529cf2579ccc06b450c...<i style={{ cursor: "pointer" }} className="fas fa-copy text-warning m-1"></i></h5>
+                                        <h5 onClick={() => handleCopy("0x18562342774d0f6db54264edcdc650a6e2c7d45c")}>0x36c236529cf2579ccc06b450c...<i style={{ cursor: "pointer" }} className="fas fa-copy text-warning m-1"></i></h5>
                                         <h4 className="card-title">Deposit Using USDT | USDT(TRC20)</h4>
                                         <p className="card-description">USD | USDT Deposit Method</p><hr />
                                             <p className="card-description">Request other available Deposit Method</p>
