d424c712e982bd423ff586e5345be426d888ec03
JamesDev <deelordpopdy2@gmail.com>
2026-07-25 03:11:42 -0700

    safe

--- OCIC_server/index.js
+++ OCIC_server/index.js
@@ -3,8 +3,33 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+const cors = require("cors");
+
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://ocicinvestment.vercel.app",
+  "https://ocicinvestment-kyc.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Database Connected successfuly!"))
